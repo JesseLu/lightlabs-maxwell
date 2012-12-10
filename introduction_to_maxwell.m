b145d568a7fdb84f4a9eb0ddbb0e6dfebfa8605b
Jesse Lu <mr.jesselu@gmail.com>
2012-12-10 15:46:32 -0800

    Updated intro.

--- introduction_to_maxwell.m
+++ introduction_to_maxwell.m
@@ -8,7 +8,7 @@
 %   You have the Matlab part of the deal, now you just need to get the cloud 
 %   part. To do that you only need to:
 %
-%       1. <a href="https://portal.aws.amazon.com/gp/aws/securityCredentials?#access_credentials">Get your Amazon Web Services (AWS) Access Keys</a> 
+%           <a href="https://portal.aws.amazon.com/gp/aws/securityCredentials?#access_credentials">Get your Amazon Web Services (AWS) Access Keys</a> 
 %           Specifically, you need your "Access Key ID" and "Secret Access Key",
 %           which will be used to launch and terminate Maxwell clusters.
 %           If you haven't signed up for an AWS account yet, you can do so at 
@@ -16,15 +16,15 @@
 %           (may need to be opened outside of Matlab).
 %           
 %   And that's it! Just a few words before we let you loose.
+%
+%       <a href="matlab:showdemo maxwell">Read the docs!</a> Full documentation including Quick-start is there.
+%
+%       Need more help? Use our Q&A site: <a href="http://ask.lightlabs.co">ask.lightlabs.co</a>.
 %   
 %       We don't mess up your system -- i.e. you need to reload Maxwell every 
 %       time you start Matlab. Do this quickly using:
 %           <a href="matlab: fprintf('')">eval(urlread('http://m.lightlabs.co/pre-release'))</a>
 %       Note: Automate by adding it to your <a href="matlab:doc startup">startup.m</a> file.
-%
-%       <a href="matlab:showdemo maxwell">Read the docs!</a> Full documentation including Quick-start is there.
-%
-%       Need more help? Use our Q&A site: <a href="http://ask.lightlabs.co">ask.lightlabs.co</a>.
 
 fprintf('\n');
 help introduction_to_maxwell
